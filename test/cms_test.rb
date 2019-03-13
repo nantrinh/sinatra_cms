1eac900b04a064a9574685e87475f33f65f7303d
Nancy Trinh <nancytrinh20@gmail.com>
2019-03-13 15:13:34 -0700

    Add markdown test

--- test/cms_test.rb
+++ test/cms_test.rb
@@ -40,4 +40,11 @@ class CMSTest < Minitest::Test
     get "/"
     refute_includes last_response.body, "notafile.ext does not exist"
   end
+
+  def test_markdown_formatting
+    get "/about.md"
+    assert_equal 200, last_response.status
+    assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
+    assert_includes last_response.body, "<h1>Ruby is...</h1>"    
+  end
 end
