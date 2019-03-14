8b239b3c1731d2ad658cc002c4769d4ddfcea3ca
Nancy Trinh <nancytrinh20@gmail.com>
2019-03-14 13:59:52 -0700

    Store usernames and passwords in yaml file

--- cms.rb
+++ cms.rb
@@ -3,6 +3,7 @@ require "sinatra/reloader"
 require "tilt/erubis"
 require "redcarpet"
 require 'fileutils'
+require 'yaml'
 
 configure do
   enable :sessions
@@ -31,6 +32,15 @@ def load_file_content(path)
   when ".md"
     erb render_markdown(content)
   end
+end
+
+def load_user_credentials
+  credentials_path = if ENV["RACK_ENV"] == "test"
+    File.expand_path("../test/users.yml", __FILE__)
+  else
+    File.expand_path("../users.yml", __FILE__)
+  end
+  YAML.load_file(credentials_path)
 end
 
 def user_signed_in?
@@ -88,8 +98,10 @@ get "/:filename/edit" do
 end
 
 post '/users/signin' do
-  if params[:username] == 'admin' && params[:password] == 'secret'
-    session[:username] = params[:username]
+  credentials = load_user_credentials
+  username = params[:username]
+  if credentials.key?(username) && credentials[username] == params[:password]
+    session[:username] = username 
     session[:message] = 'Welcome!'
     redirect '/'
   else
